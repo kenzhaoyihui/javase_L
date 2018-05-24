86cbf4b8b57147cd7f540385e590c5e8623fb6d4
Yihui Zhao <yzhao@redhat.com>
2018-05-24 08:14:51 +0800

    add the case related to removedList

---
+++ src/Collections/reflect/yzhao/RemoveListElem.java
@@ -0,0 +1,54 @@
+package Collections.reflect.yzhao;
+
+import java.util.*;
+
+public class RemoveListElem {
+
+    public static void main(String[] args){
+
+        List<String> list = new ArrayList<>();
+        list.add("aaa");
+        list.add("ccc");
+        list.add("bbb");
+        list.add("aaa");
+        list.add("aba");
+        list.add("aaa");
+
+//        Set set = new HashSet();
+//        List newList = new ArrayList();
+//
+//        for(String s: list){
+//            if(set.add(s)){
+//                newList.add(s);
+//            }
+//        }
+//        System.out.println(newList);
+
+
+
+
+//        Set set = new HashSet();
+//        List newList = new ArrayList();
+//
+//        for (String s: list){
+//            if(!newList.contains(s)){
+//                newList.add(s);
+//            }
+//        }
+//        System.out.println(newList);
+
+
+
+//        Set set = new TreeSet();
+//        List newList = new ArrayList();
+//
+//        set.addAll(list);
+//        newList.addAll(set);
+//        System.out.println(newList);
+
+
+//        List newList = new ArrayList(new HashSet(list));
+//        System.out.println(newList);
+
+    }
+}
--- src/design/pattern/java/singleton3.java
+++ src/design/pattern/java/singleton3.java
@@ -1,4 +1,92 @@
 package design.pattern.java;
 
 public class singleton3 {
+
+    /*
+    //first method: lazy men, not security
+
+    private static singleton3 instance;
+
+    private singleton3(){}
+    public static singleton3 getInstance(){
+        if(instance==null){
+            instance = new singleton3();
+        }
+
+        return instance;
+    }
+
+    */
+
+    //second method, lazy man, security
+    /*
+    private static singleton3 instance;
+
+    private singleton3(){
+    }
+
+    public static synchronized singleton3 getInstance(){
+        if(instance==null){
+            instance = new singleton3();
+        }
+
+        return instance;
+    }
+    */
+
+    //three method: hungry man
+    /*
+    private static singleton3 instance = new singleton3();
+    private singleton3(){}
+
+    public static singleton3 getInstance(){
+        return instance;
+    }
+    */
+
+    //four method: hungry man, two
+    /*
+    private static singleton3 instance = null;
+
+    static {
+        instance = new singleton3();
+    }
+
+    private singleton3(){}
+
+    public static singleton3 getInstance(){
+        return instance;
+    }
+    */
+
+
+    //five method: static inner class
+    /*
+    private static class SingletonHolder{
+        private static final singleton3 instance = new singleton3();
+    }
+
+    private singleton3(){}
+
+    public static final singleton3 getInstance(){
+        return SingletonHolder.instance;
+    }
+    */
+
+    //six method: double check
+
+    private volatile static singleton3 instance;
+    private singleton3(){
+    }
+
+    public static singleton3 getInstance(){
+        if(instance==null){
+            synchronized (singleton3.class){
+                if(instance==null){
+                    instance = new singleton3();
+                }
+            }
+        }
+        return instance;
+    }
 }
