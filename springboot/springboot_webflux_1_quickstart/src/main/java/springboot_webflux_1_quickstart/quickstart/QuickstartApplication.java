c712463ca17fa40fc4d36ad28c91b7cc6c8c125b
Yihui Zhao <yzhao@redhat.com>
2018-07-19 18:31:43 +0800

    add springboot

--- springboot/springboot_webflux_1_quickstart/src/main/java/springboot_webflux_1_quickstart/quickstart/QuickstartApplication.java
+++ springboot/springboot_webflux_1_quickstart/src/main/java/springboot_webflux_1_quickstart/quickstart/QuickstartApplication.java
@@ -2,6 +2,7 @@ package springboot_webflux_1_quickstart.quickstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class QuickstartApplication {
